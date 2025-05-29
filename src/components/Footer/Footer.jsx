e3dafe46e6ea24c0637c65cad36edcb33c2d47f8
bellion13 <ttai132002@gmail.com>
2025-05-29 17:30:40 +0700

    Done Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import './Footer.scss'
-import ft from '../../assets/images/footer.jpg'
+// import ft from '../../assets/images/footer.jpg'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/images/favicon-32x32.png';
+
 function Footer() {
     return (
         <div className='footer'>
-            <div className="footer__container">
-                <div className='bgr-footer'>
-                    <img src={ft} alt="Footer" />
+        <div className='footer__img'></div>
+            <div className="footer__content">
+                <div className="footer__content__logo">
+                    
+                    <Link to='/'> <img src={logo} alt="logo" />Ani Jikan</Link>
+                </div>
+            </div>
+            <div className="footer__content__menus">
+                <div className="footer__content__menu">
+                    <Link to='/'>Home</Link>
+                    <Link to='/'>Contact us</Link>
+                    <Link to='/'>Term of services</Link>
+                    <Link to='/'>About us</Link>
+                </div>
+                <div className="footer__content__menu">
+                    <Link to='/'>Live</Link>
+                    <Link to='/'>FAQ</Link>
+                    <Link to='/'>Premium</Link>
+                    <Link to='/'>Pravacy policy</Link>
+                </div>
+                <div className="footer__content__menu">
+                    <Link to='/'>You must watch</Link>
+                    <Link to='/'>Recent release</Link>
+                    <Link to='/'>Jikan</Link>
                 </div>
             </div>
         </div>
