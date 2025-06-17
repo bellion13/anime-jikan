119851eefe2e3177591527fe9e755bf6a892207c
bellion13 <ttai132002@gmail.com>
2025-06-17 09:46:43 +0700

    fix container

--- src/components/AnimeSlider/AnimeSlider.jsx
+++ src/components/AnimeSlider/AnimeSlider.jsx
@@ -11,7 +11,7 @@ const AnimeSlider = ({  list }) => {
   );
 
   return (
-    <div className="list-anime">
+    <div className="container">
       <div className="list-anime-bottom">
         <Swiper
           grabCursor={true}
